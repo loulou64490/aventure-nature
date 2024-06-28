e0b947508c1dbe957aee20f228d747578f994f74
Your Name <you@example.com>
2024-06-28 07:57:07 +0200

    Hop, un header de nouveau fonctionnel

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-// Fonction pour ajouter ou supprimer la classe selon la position de défilement
-function handleScroll() {
+document.addEventListener('DOMContentLoaded', () => {
     const header = document.getElementById('header');
-    if (window.scrollY > 0) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 0) {
+            header.classList.add('scrolled');
+        } else {
+            header.classList.remove('scrolled');
+        }
+    });
+});
+
+function switchCheckbox() {
+    // Récupère la case à cocher par son ID
+    var checkbox = document.getElementById("menu-btn");
+    // Bascule l'état de la case à cocher
+    checkbox.checked = false;
 }

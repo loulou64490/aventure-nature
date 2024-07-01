1fa6289b1c3c1268049a7da5f9bcd85155620948
Your Name <you@example.com>
2024-07-01 11:24:19 +0200

    scroll before : check

--- script.js
+++ script.js
@@ -30,3 +30,25 @@ function hideMenu() {
     var checkbox = document.getElementById("menu-btn");
     checkbox.checked = false;
 }
+
+document.querySelectorAll('.scroll-link').forEach(function(link) {
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        // valeur scroll
+        const offset = 50;
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            const elementPosition = targetElement.getBoundingClientRect().top;
+            const offsetPosition = elementPosition - offset;
+
+            window.scrollBy({
+                top: offsetPosition,
+                behavior: 'smooth'
+            });
+        }
+    });
+});

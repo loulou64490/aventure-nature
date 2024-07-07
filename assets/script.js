80c1a761cf751f921d2b90e40194c6f1ddb93882
Your Name <you@example.com>
2024-07-07 10:29:25 +0200

    Restore scroll before

--- assets/script.js
+++ assets/script.js
@@ -24,31 +24,31 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
     handleScroll();
+
+    document.querySelectorAll('.scroll-link').forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+    
+            // valeur scroll
+            const offset = 50;
+    
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+    
+            if (targetElement) {
+                const elementPosition = targetElement.getBoundingClientRect().top;
+                const offsetPosition = elementPosition - offset;
+    
+                window.scrollBy({
+                    top: offsetPosition,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
 });
 
 function hideMenu() {
     var checkbox = document.getElementById("menu-btn");
     checkbox.checked = false;
 }
-
-document.querySelectorAll('.scroll-link').forEach(function(link) {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        // valeur scroll
-        const offset = 50;
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            const elementPosition = targetElement.getBoundingClientRect().top;
-            const offsetPosition = elementPosition - offset;
-
-            window.scrollBy({
-                top: offsetPosition,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
